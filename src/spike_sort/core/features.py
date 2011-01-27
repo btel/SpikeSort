ad2784e7f12a7d7674a9371d6e08c337b94526ca
Bartosz Telenczuk <muchatel@poczta.fm>
2011-01-27 16:39:43 +0100

    updated docstrings

--- src/spike_sort/core/features.py
+++ src/spike_sort/core/features.py
@@ -5,7 +5,7 @@ Provides functions to calculate spike waveforms features.
 
 Functions starting with `fet` implement various features calculated
 from the spike waveshapes. They have usually one required argument
-:ref:`spikewave` structure.  
+:ref:`spikewave` structure (but there are exceptions!).  
 
 """
 
@@ -14,6 +14,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def combine(args, normalize=True):
+    """Combine features into a single structure
+    
+    :arguments:
+        * args -- a tuple of features
+    
+    :output:
+        * combined_fetures -- dictionary with two keys `data` -- feature
+          array of shape `(n_spikes, n_features)` and `names` -- feature labels
+    """
+
     features, names = zip(*args)
 
     data  = np.hstack(features)
@@ -21,11 +31,28 @@ def combine(args, normalize=True):
         data = (data-data.min(0)[np.newaxis,:])
         data = data/data.max(0)[np.newaxis, :]
 
-    ret_dict = {"data": data,
+    combined_features = {"data": data,
                 "names":np.concatenate(names)}
-    return ret_dict
+    return combined_features
 
 def PCA(data,ncomps=2):
+    """
+    Perfrom a principle component analysis on `data` and project
+    data on `ncomps` eigenvectors
+
+    :arguments:
+     
+     * data -- (n_vars, n_obs) array where `n_vars` is the number of
+       variables (vector dimensions) and `n_obs` the number of
+       observations
+
+    :output:
+
+     * evals -- sorted eigenvalues
+     * evecs -- sorted eigenvectors
+     * score -- projection of the data on `ncomps` components
+     """
+
     #norm=data/np.std(data,1)[:,np.newaxis]
     #norm[np.isnan(norm)]=0
     #norm = data
@@ -39,7 +66,18 @@ def PCA(data,ncomps=2):
     return evals,evecs,score
 
 def fetPCs(spikes_data,ncomps=2):
-    """Calculate principal components (PCs)."""
+    """Calculate principal components (PCs).
+    
+    :arguments:
+        
+     * spikes -- spikewave structures
+     * ncomps -- number of components to retain
+     
+     :output:
+
+     * pcs -- projection scores of size `(n_contacts*ncomps, n_spikes)`
+     * names -- feature labels ("Ch0:PC0', "Ch0:PC1", "Ch1:PC0", etc.)
+    """
    
     spikes = spikes_data["data"]
     def _getPCs(data):
@@ -76,7 +114,7 @@ def fetP2P(spikes_data):
      
      * name -- feature labels (ex. Ch0:P2P, Ch1:P2P)
 
-    :example:
+    **Example**
 
      We will generate a spikewave structure containing only a single
      spike on a single channel
@@ -100,6 +138,9 @@ def fetP2P(spikes_data):
     return p2p, names
 
 def fetSpIdx(spikes_data):
+    """
+    Spike sequential index (0,1,2, ...)
+    """
     spikes = spikes_data["data"]
 
     n_datapts = spikes.shape[1]
@@ -107,6 +148,15 @@ def fetSpIdx(spikes_data):
     return np.arange(n_datapts)[:, np.newaxis], ["SpIdx"]
 
 def fetSpTime(spt_dict):
+    """
+    Spike occurance time im milliseconds.
+
+    :arguments:
+        * spt_dict -- dictionary with `data` key containing spike times
+    :output:
+        * spt -- spike times
+        * label
+    """
 
     spt = spt_dict['data']
 
