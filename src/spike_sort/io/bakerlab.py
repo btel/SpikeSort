5393def2f600ae5d0ea827b988040a8d572ff450
Bartosz Telenczuk <muchatel@poczta.fm>
2011-02-01 19:24:30 +0100

    testing bakerlab module

--- src/spike_sort/io/bakerlab.py
+++ src/spike_sort/io/bakerlab.py
@@ -3,6 +3,28 @@
 
 import os
 import numpy as np
+import json
+import re
+
+_regexp="^/(?P<subject>[a-zA-z]+)/s(?P<ses_id>.+)/el(?P<el_id>[0-9]+)$"
+def read_sp(conf_file, dataset):
+    with file(conf_file) as fid:
+        conf_dict = json.load(fid)
+    m = re.match(_regexp, dataset)
+    fname = conf_dict['fspike'].format(**m.groupdict())
+    full_path = os.path.join(conf_dict['dirname'], fname)
+    sp = np.fromfile(full_path,dtype=np.int16)/200.
+    return {'data':sp, "FS":conf_dict['FS']} 
+
+def write_sp(sp_dict, conf_file, dataset):
+    sp = sp_dict['data']
+    with file(conf_file) as fid:
+        conf_dict = json.load(fid)
+    m = re.match(_regexp, dataset)
+    fname = conf_dict['fspike'].format(**m.groupdict())
+    full_path = os.path.join(conf_dict['dirname'], fname)
+    sp_int = (sp*200).astype(np.int16)
+    sp_int.tofile(full_path)
 
 def read_spt(dir_name, dataset):
     """Returns spike times in miliseconds:
--- tests/test_io.py
+++ tests/test_io.py
@@ -5,16 +5,19 @@ import tables
 import numpy as np
 import os
 import spike_sort.io.hdf5
+import spike_sort.io.bakerlab
 import filecmp
+import json
 
 class TestHDF:
     def setUp(self):
-        self.fname = 'test.h5'
+        self.data = np.random.randint(1000,size=(100,4))
+        self.spt = np.random.randint(0,100, (10,))/200.
         self.el_node = '/Subject/Session/Electrode'
+        self.fname = 'test.h5'
         self.cell_node = self.el_node+'/cell'
         self.h5f =  tables.openFile(self.fname,'a')
-        self.data = np.random.rand(100,4)
-        self.spt = np.random.randint(0,100, (10,))
+        
         self.spt.sort()
         atom = tables.Atom.from_dtype(self.data.dtype)
         shape = self.data.shape
@@ -51,3 +54,43 @@ class TestHDF:
     def test_read_spt(self):
         spt = spike_sort.io.hdf5.read_spt(self.fname, self.cell_node)
         ok_((spt['data']==self.spt).all())
+        
+class TestBakerlab:
+    def setup(self):
+        file_descr = {"fspike":"{ses_id}{el_id}.sp",
+                      "dirname":".",
+                      "FS":5.E3}
+        self.el_node = '/Test/s32test01/el1'
+        self.data = np.random.randint(-1000, 1000, (100,))/200.
+        self.spt = np.random.randint(0,100, (10,))
+        self.conf_file = 'test.conf'
+        self.fname = "32test011.sp"
+        
+        with open(self.conf_file, 'w') as fp:
+             json.dump(file_descr, fp)
+          
+        (self.data*200).astype(np.int16).tofile(self.fname)
+        
+    def tearDown(self):
+        os.unlink(self.conf_file)
+        os.unlink(self.fname)
+        
+    def test_write_spt(self):
+        pass
+    def test_read_spt(self):
+        
+        pass
+    def test_write_sp(self):
+        el_node_tmp = '/Test/s32test01/el2'
+        spike_sort.io.bakerlab.write_sp({"data":self.data}, self.conf_file,
+                                       el_node_tmp)
+        files_eq = filecmp.cmp("32test011.sp","32test012.sp", shallow=0)
+        os.unlink("32test012.sp")
+        ok_(files_eq)
+        
+    def test_read_sp(self):
+        sp = spike_sort.io.bakerlab.read_sp(self.conf_file,
+                                            self.el_node)
+        read_data = sp['data']
+        ok_((np.abs(read_data-self.data)<=1/200.).all())
+        
