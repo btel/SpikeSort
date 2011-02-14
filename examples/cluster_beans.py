12749bb443f93224ff0f608f5be853ccbb47f063
Bartosz Telenczuk <muchatel@poczta.fm>
2011-02-14 14:50:04 +0100

    added export to cluster_beans script

--- examples/cluster_beans.py
+++ examples/cluster_beans.py
@@ -3,33 +3,27 @@ import numpy as np
 import time
 
 dataset = "/Gollum/s39gollum01/el1"
-conf_file = "../data/gollum.inf"
+conf_file = "../data/gollum_export.inf"
 sp_win = [-0.2, 0.8]
 
-class PrintLabel(base.Component):
-    cluster = base.RequiredFeature("LabelSource")
-    def update(self):
-        start = time.time()
-        labels =  self.cluster.labels
-        duration = time.time() - start
-        
-        print "Cell labels:", labels, "No. labels:", len(labels), "Calc. time:", duration
-        
-
-base.features.Provide("SignalSource",      components.BakerlabSource(conf_file, dataset))
-base.features.Provide("SpikeMarkerSource", components.SpikeDetector(contact=3, thresh='auto'))
+io_filter = components.BakerlabSource(conf_file, dataset)
+base.features.Provide("SignalSource",      io_filter)
+base.features.Provide("SpikeMarkerSource", components.SpikeDetector(contact=3, 
+                                                                    thresh='auto'))
 base.features.Provide("SpikeSource",       components.SpikeExtractor(sp_win=sp_win))
 base.features.Provide("FeatureSource",     components.FeatureExtractor())
 base.features.Provide("LabelSource",       components.ClusterAnalyzer("gmm", 5))
-base.features.Provide("Print",             PrintLabel())
+base.features.Provide("EventsOutput",      io_filter)
 
 plot1 = components.PlotFeatures()
 plot2 = components.PlotSpikes()
 legend = components.Legend()
+export = components.ExportCells()
 
 base.features["FeatureSource"].add_feature("P2P")
 base.features["FeatureSource"].add_feature("PCs", ncomps=1)
 
-plot1.show()
-plot2.show()
-legend.show()
+export.export()
+#plot1.show()
+#plot2.show()
+#legend.show()
