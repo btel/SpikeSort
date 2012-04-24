5ddea77948a075bdbe46ed8cae169a32fe2a0ea8
Phillip Cloud <cpcloud@gmail.com>
2012-04-24 00:21:03 -0400

    removed unused imports and cleaned up path code

--- examples/analysis/cell_xcorr.py
+++ examples/analysis/cell_xcorr.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-import numpy as np
+import os
+import itertools
+
 import matplotlib.pyplot as plt
+
 from spike_sort.io.filters import BakerlabFilter
 from spike_analysis import io_tools, xcorr
 
 cell_pattern = "/Gollum/s4gollum*/el*/cell*"
-filter = BakerlabFilter("../../data/gollum.inf") 
+path = os.path.join(os.pardir, os.pardir, 'data', 'gollum.inf')
+filt = BakerlabFilter(path)
 
 if __name__ == "__main__":
-
-    cell_nodes = io_tools.list_cells(filter, cell_pattern)
-    cells = [io_tools.read_dataset(filter, node) for node in cell_nodes]
-    for data, name in zip(cells, cell_nodes):
-        data['dataset']=name
+    cell_nodes = io_tools.list_cells(filt, cell_pattern)
+    cells = [io_tools.read_dataset(filt, node) for node in cell_nodes]
+    for data, name in itertools.izip(cells, cell_nodes):
+        data['dataset'] = name
     xcorr.show_xcorr(cells)
     plt.show()
 
