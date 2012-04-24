9802b7789c88fc10c7f39adfdd6d38ddf7bfe964
Phillip Cloud <cpcloud@gmail.com>
2012-04-24 00:21:03 -0400

    removed unused imports and cleaned up path code

--- examples/analysis/cell_dashboard.py
+++ examples/analysis/cell_dashboard.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-import numpy as np
+import os
 import matplotlib.pyplot as plt
-
-import sys
 
 from spike_analysis import dashboard
 from spike_sort.io.filters import BakerlabFilter
 
 if __name__ == "__main__":
     cell = "/Gollum/s39gollum03/el1/cell1"
-    filter = BakerlabFilter("../../data/gollum_export.inf") 
+    path = os.path.join(os.pardir, os.pardir, 'data', 'gollum_export.inf')
+    filt = BakerlabFilter(path)
     
-    dashboard.show_cell(filter, cell)
+    dashboard.show_cell(filt, cell)
 
     plt.show()
 
