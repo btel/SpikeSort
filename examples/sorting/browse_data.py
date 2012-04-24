807dc8098ae7df4432af9e9d4a013ca2ce1221e4
Phillip Cloud <cpcloud@gmail.com>
2012-04-24 00:17:03 -0400

    got rid of unused imports; browse_data.py cluster_auto.py cluster_manual.py now pep8 conformant

--- examples/sorting/browse_data.py
+++ examples/sorting/browse_data.py
@@ -10,22 +10,18 @@ Keyboard shortcuts:
 """
 
 import spike_sort as sort
-from spike_sort.io.filters import PyTablesFilter, BakerlabFilter
+from spike_sort.io.filters import PyTablesFilter
 from spike_sort.ui import spike_browser
 import os
 
 DATAPATH = os.environ['DATAPATH']
 
 if __name__ == "__main__":
-
     dataset = "/SubjectA/session01/el1"
     data_fname = os.path.join(DATAPATH, "tutorial.h5") 
     
     io_filter = PyTablesFilter(data_fname)
     sp = io_filter.read_sp(dataset) 
-    spt = sort.extract.detect_spikes(sp,  contact=3,
-                                     thresh='auto')
+    spt = sort.extract.detect_spikes(sp, contact=3, thresh='auto')
     
-    spike_browser.browse_data_tk(sp, spt, win=50)
-    
-    
+    spike_browser.browse_data_tk(sp, spt, win=50)   
