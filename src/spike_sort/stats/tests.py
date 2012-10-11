9e6fd516e463f81645b8d797885bbb34e31e55ab
belevtsoff <belevtsoff@gmail.com>
2012-10-11 18:47:04 +0200

    added spike_sort/stats with fortran extension

--- src/spike_sort/stats/tests.py
+++ src/spike_sort/stats/tests.py
@@ -1,4 +1,5 @@
-#import _diptst
+#from spike_sort.stats._diptst import diptst1
+import _diptst
 import numpy as np
 from scipy import stats as st
 
@@ -8,9 +9,9 @@ def mean_r(data):
 def std_r(data):
     return np.median(np.abs(data - mean_r(data)[:, None]), 1)/0.6745
 
-#def dip1d(data):
-    #sdata = np.sort(data)
-    #return _diptst.diptst1(sdata)[0]
+def dip1d(data):
+    sdata = np.sort(data)
+    return _diptst.diptst1(sdata)[0]
 
 def ks1d(data):
     mr = np.median(data)
@@ -18,8 +19,8 @@ def ks1d(data):
 
     return st.kstest(data, st.norm(loc=mr, scale=stdr).cdf)[0]
 
-#def dip(data):
-    #return np.array([dip1d(x) for x in data])[:, None]
+def dip(data):
+    return np.array([dip1d(x) for x in data])[:, None]
 
 def ks(data):
     return np.array([ks1d(x) for x in data])
