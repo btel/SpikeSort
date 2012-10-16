b13851ada507085ab69707d42ab2ded073f2032b
belevtsoff <belevtsoff@gmail.com>
2012-10-16 15:40:34 +0200

    all tests from stats now have axis parameter

--- src/spike_sort/stats/tests.py
+++ src/spike_sort/stats/tests.py
@@ -1,26 +1,54 @@
-#from spike_sort.stats._diptst import diptst1
 import _diptst
 import numpy as np
 from scipy import stats as st
 
-def mean_r(data):
-    """Computes robust estimate of mean (Quiroga et al, 2004) row-wise
+def multidimensional(func1d):
+    """apply 1d function along specified axis
+    """
+    def _decorated(data, axis = 0):
+        if data.ndim <= 1:
+            return func1d(data)
+        else:
+            return np.apply_along_axis(func1d, axis, data)
+    _decorated.__doc__ = func1d.__doc__
+
+    return _decorated
+
+def unsqueeze(data, axis):
+    """inserts new axis to data at position `axis`.
+    
+    This is very useful when one wants to do operations which support
+    broadcasting without using np.newaxis every time.
 
     Parameters
     ----------
     data : array
-        (n_vecs, n_obs) input data array
+        input array
+    axis : int
+        axis to be inserted
 
     Returns
     -------
-    data : array
-        (n_vecs) row-wise mean values
+    out : array
+        array which has data.ndim+1 dimensions. Additional dimension
+        has length 1
+    
+    Example
+    -------
+    >>> data = [[1,2,3], [4,5,6]]
+    >>> m = np.mean(data, 1)
+    >>> data -= unsqueeze(m, 1)
+    >>> data.mean(1)
+    array([ 0.,  0.])
+    
     """
-    return np.median(data, 1)
+    shape = data.shape
+    shape = np.insert(shape, axis, 1)
+    return np.reshape(data, shape)
 
-def std_r(data):
+def std_r(data, axis = 0):
     """Computes robust estimate of standard deviation
-    (Quiroga et al, 2004) row-wise
+    (Quiroga et al, 2004)
 
     Parameters
     ----------
@@ -32,36 +60,44 @@ def std_r(data):
     data : array
         (n_vecs) row-wise std values
     """
-    return np.median(np.abs(data - mean_r(data)[:, None]), 1)/0.6745
+    median = unsqueeze(np.median(data, axis), axis)
+    std_r = np.median(np.abs(data - median), axis)/0.6745
+    return std_r
 
-def dip1d(data):
+@multidimensional
+def dip(data):
     """Computes DIP statistic (Hartigan & Hartigan 1985)
 
     Parameters
     ----------
     data : array
-        (n_vecs) input data array
+        input data array
+    axis : int
+        axis along which to compute dip
 
     Returns
     -------
-    data : float
-        DIP statistic
+    data : float or array
+        DIP statistic. If the input data is flat, returns float
     """
     sdata = np.sort(data)
     return _diptst.diptst1(sdata)[0]
 
-def ks1d(data):
+@multidimensional
+def ks(data):
     """Computes Kolmogorov-Smirnov statistic (Lilliefors modification)
 
     Parameters
     ----------
     data : array
         (n_vecs) input data array
+    axis : int
+        axis along which to compute ks
 
     Returns
     -------
-    data : float
-        KS statistic
+    data : array or float
+        KS statistic. If the input data is flat, returns float
     """
     mr = np.median(data)
     stdr = np.median(np.abs(data - mr))/0.6745
@@ -72,47 +108,12 @@ def ks1d(data):
 
     return st.kstest(data, st.norm(loc=mr, scale=stdr).cdf)[0]
 
-def dip(data):
-    """Computes DIP statistic row-wise (see also dip1d)
+std = np.std
 
-    Parameters
-    ----------
-    data : array
-        (n_vecs, n_obs) input data array
 
-    Returns
-    -------
-    data : array
-        DIP statistic row-wise
-    """
-    return np.array([dip1d(x) for x in data])
 
-def ks(data):
-    """Computes KS statistic row-wise (see also ks1d)
 
-    Parameters
-    ----------
-    data : array
-        (n_vecs, n_obs) input data array
 
-    Returns
-    -------
-    data : array
-        KS statistic row-wise
-    """
-    return np.array([ks1d(x) for x in data])
 
-def std(data):
-    """Computes standard deviation row-wise
 
-    Parameters
-    ----------
-    data : array
-        (n_vecs, n_obs) input data array
 
-    Returns
-    -------
-    data : array
-        std row-wise
-    """
-    return np.std(data, 1)
