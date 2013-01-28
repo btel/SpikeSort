ddfd54c322d95445a678f6a0acd63849ecc641b7
Bartosz Telenczuk <muchatel@poczta.fm>
2013-01-28 15:25:29 +0100

    core modules: improved complience with pep8

--- src/spike_sort/core/evaluate.py
+++ src/spike_sort/core/evaluate.py
@@ -2,21 +2,24 @@
 #coding=utf-8
 
 import numpy as np
-import extract, cluster
+import extract
+import cluster
 import warnings
+
 
 def deprecation(message):
     warnings.warn(message, DeprecationWarning, stacklevel=2)
+
 
 def snr_spike(spike_waves, scale=5.):
     """Estimate signal-to-noise ratio (SNR) as a ratio of
     peak-to-peak amplitude of an average spike to the std. deviation
     of residuals
-    
+
     Parameters
     ----------
     spike_waves : dict
-    
+
     Returns
     -------
     snr : float
@@ -30,17 +33,18 @@ def snr_spike(spike_waves, scale=5.):
     snr = peak_to_peak / (noise_std * scale)
     return snr
 
+
 def snr_clust(spike_waves, noise_waves):
     """Calculate signal-to-noise ratio.
-    
+
     Comparing average P2P amplitude of spike cluster to noise cluster
     (randomly selected signal segments)
-    
+
     Parameters
     ----------
     spike_waves : dict
     noise_waves : dict
-    
+
     Returns
     -------
     snr : float
@@ -58,53 +62,53 @@ def snr_clust(spike_waves, noise_waves):
 
     return snr
 
-def extract_noise_cluster(sp, spt, sp_win, type="positive"):
 
+def extract_noise_cluster(sp, spt, sp_win, type="positive"):
 
     deprecation("extract_noise_cluster deprecated. Use"
                 " detect_noise and extract_spikes instead.")
-    spt_noise = detect_noise_spikes(sp, spt, sp_win, type)
+    spt_noise = detect_noise(sp, spt, sp_win, type)
     sp_waves = extract.extract_spikes(sp, spt_noise, sp_win)
 
     return sp_waves
+
 
 def rand_sample_spt(spt, max_spikes):
     n_spikes = len(spt['data'])
     spt_data = spt['data']
     spt_new = spt.copy()
-    if max_spikes and n_spikes>max_spikes:
+    if max_spikes and n_spikes > max_spikes:
         i = np.random.rand(n_spikes).argsort()[:max_spikes]
         spt_new['data'] = spt_data[i]
     return spt_new
 
 
 def detect_noise(sp, spt, sp_win, type="positive", max_spikes=None,
-        resample=1):
+                 resample=1):
     """Find noisy spikes"""
 
     spike_waves = extract.extract_spikes(sp, spt, sp_win)
-    
+
     if type == "positive":
         threshold = calc_noise_threshold(spike_waves, 1)
         spt_noise = extract.detect_spikes(sp, threshold, 'rising')
         spt_noise = rand_sample_spt(spt_noise, max_spikes)
         spt_noise = extract.remove_spikes(spt_noise, spt, sp_win)
         spt_noise = extract.align_spikes(sp, spt_noise, sp_win, 'max',
-                resample=resample)
+                                         resample=resample)
     else:
         threshold = calc_noise_threshold(spike_waves, -1)
         spt_noise = extract.detect_spikes(sp, threshold, 'falling')
         spt_noise = rand_sample_spt(spt_noise, max_spikes)
         spt_noise = extract.remove_spikes(spt_noise, spt, sp_win)
         spt_noise = extract.align_spikes(sp, spt_noise, sp_win, 'min',
-                resample=resample)
-
-
+                                         resample=resample)
     return spt_noise
+
 
 def calc_noise_threshold(spike_waves, sign=1, frac_spikes=0.02, frac_max=0.5):
     """ Find threshold to extract noise cluster.
-    
+
     According to algorithm described in Joshua et al. (2007)
 
     Parameters
@@ -115,8 +119,8 @@ def calc_noise_threshold(spike_waves, sign=1, frac_spikes=0.02, frac_max=0.5):
 
     sign : int
         sign should be negative for negative-going spikes and positive for
-        postitive-going spikes. Note that only sign of this number is taken into
-        account.
+        postitive-going spikes. Note that only sign of this number is
+        taken into account.
 
     frac_spikes : float, optional
         fraction of largest (smallest) spikes to calculate the threshold
@@ -129,12 +133,11 @@ def calc_noise_threshold(spike_waves, sign=1, frac_spikes=0.02, frac_max=0.5):
     Returns
     -------
     threshold : float
-        threshold to obtain a noise cluster 
-    """
+        threshold to obtain a noise cluster"""
 
     gain = np.sign(sign)
 
-    peak_amp = np.max(gain*spike_waves['data'],0)     
+    peak_amp = np.max(gain*spike_waves['data'], 0)
     frac_spikes = 0.02
     frac_max = 0.5
     peak_amp.sort()
@@ -143,17 +146,22 @@ def calc_noise_threshold(spike_waves, sign=1, frac_spikes=0.02, frac_max=0.5):
 
     return threshold
 
-def isolation_score(sp, spt, sp_win, spike_type='positive', lam=10., max_spikes=None):
+
+def isolation_score(sp, spt, sp_win, spike_type='positive', lam=10.,
+                    max_spikes=None):
     "calculate spike isolation score from raw data and spike times"
-    
+
     spike_waves = extract.extract_spikes(sp, spt, sp_win)
     spt_noise = detect_noise(sp, spt, sp_win, spike_type)
     noise_waves = extract.extract_spikes(sp, spt_noise, sp_win)
 
     iso_score = calc_isolation_score(spike_waves, noise_waves,
-            spike_type, lam=lam, max_spikes=max_spikes)
+                                     spike_type,
+                                     lam=lam,
+                                     max_spikes=max_spikes)
 
     return iso_score
+
 
 def _iso_score_dist(dist, lam, n_spikes):
 
@@ -165,7 +173,7 @@ def _iso_score_dist(dist, lam, n_spikes):
         NxM matrix, where N is number of spikes and M is number of all
         events (spikes + noise)
 
-    lam : float 
+    lam : float
         lambda parameter
 
     n_spikes : int
@@ -176,28 +184,26 @@ def _iso_score_dist(dist, lam, n_spikes):
     isolation_score : float
     """
 
-
     distSS = dist[:, :n_spikes]
-    distSN = dist[:, n_spikes:] 
+    distSN = dist[:, n_spikes:]
 
     d0 = distSS.mean()
-    expSS = np.exp(-distSS*lam*1./d0);
-    expSN = np.exp(-distSN*lam*1./d0);
- 
-    sumSS = np.sum(expSS - np.eye(n_spikes),1)
-    sumSN = np.sum(expSN, 1) 
+    expSS = np.exp(-distSS*lam*1./d0)
+    expSN = np.exp(-distSN*lam*1./d0)
 
-    correctProbS = sumSS /  (sumSS + sumSN)
+    sumSS = np.sum(expSS - np.eye(n_spikes), 1)
+    sumSN = np.sum(expSN, 1)
+
+    correctProbS = sumSS / (sumSS + sumSN)
     isolation_score = correctProbS.mean()
 
     return isolation_score
 
 
-
 def calc_isolation_score(spike_waves, noise_waves, spike_type='positive',
-        lam=10., max_spikes=None):
+                         lam=10., max_spikes=None):
     """Calculate isolation index according to Joshua et al. (2007)
-    
+
     Parameters
     ----------
     spike_waves : dict
@@ -208,8 +214,8 @@ def calc_isolation_score(spike_waves, noise_waves, spike_type='positive',
         indicates if the spikes occuring at time points given by spt are
         positive or negative going
     lambda : float
-        determines the "softness" of clusters 
-      
+        determines the "softness" of clusters
+
     Returns
     -------
     isolation_score : float
@@ -219,26 +225,24 @@ def calc_isolation_score(spike_waves, noise_waves, spike_type='positive',
 
     #Memory issue: sample spikes if too many
     if max_spikes is not None:
-        if spike_waves['data'].shape[1]>max_spikes:
-           i = np.random.rand(max_spikes).argsort()
-           spike_waves = spike_waves.copy()
-           spike_waves['data'] = spike_waves['data'][:, i]
-        if noise_waves['data'].shape[1]>max_spikes:
-           i = np.random.rand(max_spikes).argsort()
-           noise_waves = noise_waves.copy()
-           noise_waves['data'] = noise_waves['data'][:, i]
-
+        if spike_waves['data'].shape[1] > max_spikes:
+            i = np.random.rand(max_spikes).argsort()
+            spike_waves = spike_waves.copy()
+            spike_waves['data'] = spike_waves['data'][:, i]
+        if noise_waves['data'].shape[1] > max_spikes:
+            i = np.random.rand(max_spikes).argsort()
+            noise_waves = noise_waves.copy()
+            noise_waves['data'] = noise_waves['data'][:, i]
 
     n_spikes = spike_waves['data'].shape[1]
-    
+
     #calculate distance between spikes and all other events
     all_waves = {'data': np.concatenate((spike_waves['data'],
-                                    noise_waves['data']),1)}
+                                        noise_waves['data']), 1)}
     dist_matrix = cluster.dist_euclidean(spike_waves, all_waves)
     #d_0 = dist_matrix[:,:n_spikes].mean()
-   
+
     isolation_score = _iso_score_dist(dist_matrix, lam,
-            n_spikes)
+                                      n_spikes)
 
     return isolation_score
-
