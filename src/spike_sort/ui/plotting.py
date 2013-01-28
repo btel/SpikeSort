7c132ae4181c72e88e498da97d4ca9ceed27ea94
Bartosz Telenczuk <muchatel@poczta.fm>
2013-01-28 15:44:44 +0100

    pep8: ui modules

--- src/spike_sort/ui/plotting.py
+++ src/spike_sort/ui/plotting.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -11,19 +11,21 @@ from matplotlib.pyplot import show, figure, close
 import spike_sort
 cmap = plt.cm.jet
 
+
 def label_color(labels):
     """Map labels to number range [0, 1]"""
-    
-    num_labels = np.linspace(0,1., len(labels))
+
+    num_labels = np.linspace(0, 1., len(labels))
     mapper = dict(zip(labels, num_labels))
-    
+
     @np.vectorize
     def map_func(lab):
         return mapper[lab]
-    
+
     def color_func(lab):
         return cmap(map_func(lab))
     return color_func
+
 
 def plot_spikes(spikes, clust_idx=None, show_cells='all', **kwargs):
     """Plot Spike waveshapes
@@ -44,64 +46,64 @@ def plot_spikes(spikes, clust_idx=None, show_cells='all', **kwargs):
     lines_segments : object
         matplotlib line collection of spike waveshapes
     """
-    
+
     if clust_idx is None:
-        spikegraph(spikes,**kwargs)
+        spikegraph(spikes, **kwargs)
     else:
         spikes_cell = spike_sort.extract.split_cells(spikes, clust_idx)
-        
+
         if show_cells == 'all':
             labs = spikes_cell.keys()
         else:
             labs = show_cells
-       
+
         color_func = label_color(spikes_cell.keys())
         for l in labs:
             spikegraph(spikes_cell[l], color_func(l), **kwargs)
-    
-def spikegraph(spike_data, color='k', alpha=0.2, n_spikes='all', contacts='all', 
-                plot_avg=True, fig=None):
 
 
-    spikes =  spike_data['data']
+def spikegraph(spike_data, color='k', alpha=0.2, n_spikes='all',
+               contacts='all', plot_avg=True, fig=None):
+
+    spikes = spike_data['data']
     time = spike_data['time']
 
     if contacts == 'all':
         contacts = np.arange(spikes.shape[2])
-    
+
     n_pts = len(time)
-    
-    if  not n_spikes=='all':
-        spikes = spikes[:,:n_spikes,:]
+
+    if not n_spikes == 'all':
+        spikes = spikes[:, :n_spikes, :]
     n_spikes = spikes.shape[1]
     if fig is None:
         fig = plt.gcf()
     line_segments = []
-    for i, contact_id in enumerate(contacts): 
-        ax = fig.add_subplot(2,2, i+1)
-        #ax.set_xlim(time.min(), time.max())
-        #ax.set_ylim(spikes.min(), spikes.max())
-        
-        
+    for i, contact_id in enumerate(contacts):
+        ax = fig.add_subplot(2, 2, i + 1)
+        # ax.set_xlim(time.min(), time.max())
+        # ax.set_ylim(spikes.min(), spikes.max())
+
         segs = np.zeros((n_spikes, n_pts, 2))
-        segs[:,:,0] = time[np.newaxis,:]
-        segs[:,:,1] = spikes[:,:, contact_id].T
-        collection = LineCollection(segs,colors=color,
-                                            alpha=alpha)
+        segs[:, :, 0] = time[np.newaxis, :]
+        segs[:, :, 1] = spikes[:, :, contact_id].T
+        collection = LineCollection(segs, colors=color,
+                                    alpha=alpha)
         line_segments.append(collection)
         ax.add_collection(collection, autolim=True)
-        
+
         if plot_avg:
-            spikes_mean = spikes[:, :, i].mean(1) 
-            ax.plot(time, spikes_mean, color='w',lw=3)
-            ax.plot(time, spikes_mean, color=color,lw=2)
+            spikes_mean = spikes[:, :, i].mean(1)
+            ax.plot(time, spikes_mean, color='w', lw=3)
+            ax.plot(time, spikes_mean, color=color, lw=2)
         ax.autoscale_view(tight=True)
-            
+
     return line_segments
+
 
 def plot_features(features, clust_idx=None, show_cells='all', **kwargs):
     """Plot features and their histograms
-    
+
     Parameters
     ----------
     features_dict : dict
@@ -111,23 +113,24 @@ def plot_features(features, clust_idx=None, show_cells='all', **kwargs):
         each spike was classfied
     show_cells : list or 'all'
         list of identifiers of clusters (cells) to plot
-    
+
     """
 
     if clust_idx is None:
-        featuresgraph(features,**kwargs)
+        featuresgraph(features, **kwargs)
     else:
         features_cell = spike_sort.features.split_cells(features, clust_idx)
-        
+
         if show_cells == 'all':
             labs = features_cell.keys()
         else:
             labs = show_cells
-       
+
         color_func = label_color(features_cell.keys())
         for l in labs:
-            featuresgraph(features_cell[l], color_func(l),**kwargs)
-    
+            featuresgraph(features_cell[l], color_func(l), **kwargs)
+
+
 def featuresgraph(features_dict, color='k', size=1, datarange=None, fig=None):
 
     features = features_dict['data']
@@ -136,22 +139,22 @@ def featuresgraph(features_dict, color='k', size=1, datarange=None, fig=None):
     _, n_feats = features.shape
     if fig is None:
         fig = plt.gcf()
-    axes = [[fig.add_subplot(n_feats, n_feats, i*n_feats + j + 1) 
+    axes = [[fig.add_subplot(n_feats, n_feats, i * n_feats + j + 1)
              for i in range(n_feats)] for j in range(n_feats)]
     for i in range(n_feats):
         for j in range(n_feats):
             ax = axes[i][j]
-            if i!=j:
-                ax.plot(features[:,i],
-                         features[:,j],".", 
+            if i != j:
+                ax.plot(features[:, i],
+                        features[:, j], ".",
                         color=color, markersize=size)
                 if datarange:
                     ax.set_xlim(datarange)
-                    
+
             else:
                 ax.set_frame_on(False)
-           
-                ax.hist(features[:,i],20, 
+
+                ax.hist(features[:, i], 20,
                         datarange, ec="none", fc=color,
                         alpha=0.5, normed=True)
                 if datarange:
@@ -165,29 +168,28 @@ def featuresgraph(features_dict, color='k', size=1, datarange=None, fig=None):
             ax.yaxis.label.set_visible(False)
 
     for i in range(n_feats):
-        ax = axes[i][0] 
+        ax = axes[i][0]
         ax.xaxis.label.set_visible(True)
         ax = axes[0][i]
         ax.yaxis.label.set_visible(True)
 
+
 def legend(labels, colors=None, ax=None):
-    
+
     if ax is None:
-        ax=plt.gca()
+        ax = plt.gca()
     if colors is None:
         color_func = label_color(labels)
         colors = [color_func(i) for i in labels]
-        
+
     ax.set_frame_on(False)
     n_classes = len(labels)
-    x, y =np.zeros(n_classes)+0.4, 0.1*np.arange(n_classes)
-    ax.scatter(x, y, c=colors, marker='s',edgecolors="none",s=100)
-    ax.set_xlim([0,1])
+    x, y = np.zeros(n_classes) + 0.4, 0.1 * np.arange(n_classes)
+    ax.scatter(x, y, c=colors, marker='s', edgecolors="none", s=100)
+    ax.set_xlim([0, 1])
     ax.set_xticks([])
     ax.set_yticks([])
 
     for i, l in enumerate(labels):
-        ax.text(x[i]+0.1, y[i], "Cell {0}".format(l), va='center', ha='left',
-                 transform=ax.transData)
-    
-
+        ax.text(x[i] + 0.1, y[i], "Cell {0}".format(l), va='center', ha='left',
+                transform=ax.transData)
--- src/spike_sort/ui/zoomer.py
+++ src/spike_sort/ui/zoomer.py
@@ -1,59 +1,59 @@
 class Zoomer(object):
     '''Allows to zoom subplots'''
     def __init__(self, plt, fig):
-        self.axlist=[]
-        
-        self.zoomed_state={'geometry':(1,1,1),
-                           'xlabel_visible':True,
-                           'ylabel_visible':True}
-        
-        self.old_state={'geometry':[1,1,1],
-                        'xlabel_visible':True,
-                        'ylabel_visible':True}
-        
+        self.axlist = []
+
+        self.zoomed_state = {'geometry': (1, 1, 1),
+                             'xlabel_visible': True,
+                             'ylabel_visible': True}
+
+        self.old_state = {'geometry': [1, 1, 1],
+                          'xlabel_visible': True,
+                          'ylabel_visible': True}
+
         plt.connect('key_press_event', self.zoom)
-        self.fig=fig
-    
+        self.fig = fig
+
     def zoom(self, event):
-        axis=event.inaxes
-        
-        if axis==None or event.key!='z':
+        axis = event.inaxes
+
+        if axis is None or event.key != 'z':
             return
-        
+
         if axis.get_geometry() == self.zoomed_state['geometry']:
-            zoomed=True
+            zoomed = True
         else:
-            zoomed=False
-        
-        if zoomed==False:
-            #saving previous state
-            self.old_state['geometry']=axis.get_geometry()
-            self.old_state['xlabel_visible']=axis.xaxis.label.get_visible()
-            self.old_state['ylabel_visible']=axis.yaxis.label.get_visible()
-            
-            #removing old axes
-            self.axlist=list(self.fig.get_axes())
+            zoomed = False
+
+        if not zoomed:
+            # saving previous state
+            self.old_state['geometry'] = axis.get_geometry()
+            self.old_state['xlabel_visible'] = axis.xaxis.label.get_visible()
+            self.old_state['ylabel_visible'] = axis.yaxis.label.get_visible()
+
+            # removing old axes
+            self.axlist = list(self.fig.get_axes())
             for ax in self.axlist:
                 self.fig.delaxes(ax)
-            
-            #modifying state (zooming)
+
+            # modifying state (zooming)
             axis.change_geometry(*self.zoomed_state['geometry'])
             axis.xaxis.label.set_visible(self.zoomed_state['xlabel_visible'])
             axis.yaxis.label.set_visible(self.zoomed_state['ylabel_visible'])
             self.fig.add_axes(axis)
             self.fig.show()
-            
+
         else:
-            #removing current axes
+            # removing current axes
             self.fig.delaxes(axis)
-            
-            #bringing the old state back
+
+            # bringing the old state back
             axis.change_geometry(*self.old_state['geometry'])
             axis.xaxis.label.set_visible(self.old_state['xlabel_visible'])
             axis.yaxis.label.set_visible(self.old_state['ylabel_visible'])
-            
-            #adding old axes
+
+            # adding old axes
             for ax in self.axlist:
                 self.fig.add_axes(ax)
-            
+
             self.fig.show()
