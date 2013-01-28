fce3c0db9f23bbafe787dfc0e2b9cf04833d43b3
Bartosz Telenczuk <muchatel@poczta.fm>
2013-01-28 17:08:23 +0100

    registered with PyPI (closes #79)

--- setup.py
+++ setup.py
@@ -11,13 +11,25 @@ diptst_ext = Extension(name = 'spike_sort.stats._diptst',
 
 
 setup(name='SpikeSort',
-      version='0.1',
+      version='0.13',
       description='Python Spike Sorting Package',
-      author='Bartosz Telenczuk',
+      long_description="""SpikeSort is a flexible spike sorting framework 
+      implemented entirely  in Python based on widely-used packages such as numpy,
+      PyTables and matplotlib. It features manual and automatic clustering, many 
+      data formats and it is memory-efficient.""",
+      author='Bartosz Telenczuk and Dmytro Bielievtsov',
       author_email='bartosz.telenczuk@gmail.com',
-      url='http://neuroscience.telenczuk.pl',
+      url='http://spikesort.org',
       ext_modules = [diptst_ext],
-
+      classifiers = [
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "Environment :: X11 Applications",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.7"
+          ],
       packages=['spike_sort', 
                 'spike_sort.core', 
                 'spike_sort.stats',
